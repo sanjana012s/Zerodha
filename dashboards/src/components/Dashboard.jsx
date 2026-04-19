9da330a817b40e423311d7abdad339eeea13d440
sanjana012s <sanjana081220@gmail.com>
2026-04-19 09:55:53 +0530

    added dashboards

---
+++ dashboards/src/components/Dashboard.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+
+import Apps from "./Apps";
+import Funds from "./Funds";
+import Holdings from "./Holdings";
+import Menu from "./Menu";
+import Orders from "./Orders";
+import Positions from "./Positions";
+import Summary from "./Summary";
+import WatchList from "./WatchList";
+import { GeneralContextProvider } from "./GeneralContext";
+
+const Dashboard = () => {
+  return (
+    <GeneralContextProvider>
+      <div className="dashboard-container">
+        <Menu />
+        <WatchList />
+        <div className="content">
+          <Routes>
+            <Route path="/" element={<Summary />} />
+            <Route path="orders" element={<Orders />} />
+            <Route path="holdings" element={<Holdings />} />
+            <Route path="positions" element={<Positions />} />
+            <Route path="funds" element={<Funds />} />
+            <Route path="apps" element={<Apps />} />
+          </Routes>
+        </div>
+      </div>
+    </GeneralContextProvider>
+  );
+};
+
+export default Dashboard;
